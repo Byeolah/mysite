b2ae77b8b2ff525780a3201dbb9f839e6da2ca13
Byeolah <patrycja.koziol17@gmail.com>
2020-07-29 17:40:46 +0200

    Admin panel + database

--- tripus/models.py
+++ tripus/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Trip(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=50, blank=False)
+    city = models.CharField(max_length=50, blank=True)
+    departure_date = models.DateField(blank=False)
+    comeback_date = models.DateField(blank=False)
+    target = models.CharField(max_length=50, blank=False)
